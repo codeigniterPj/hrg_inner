d617972cd0d6de86039769dfcc5f49a7c58003ff
zc55803903 <549783842@qq.com>
2014-08-22 09:52:52 +0800

    0822

--- application/controllers/meal.php
+++ application/controllers/meal.php
@@ -94,8 +94,12 @@
 
  	function meal_book_confirm()
  	{	
- 		$starttime = strtotime(date("Y-m-d" , time()) . "11:00:00");
- 		$endtime = strtotime(date("Y-m-d" , time()) . "11:40:00");
+ 		$time_str = $this->readfromfile("time.config");
+ 		$time_arr = explode(",", $time_str);
+ 		$start_str = $time_arr[0];
+ 		$end_str = $time_arr[1];
+ 		$starttime = strtotime(date("Y-m-d" , time()) . $start_str);
+ 		$endtime = strtotime(date("Y-m-d" , time()) . $end_str);
  		//$nowtime = strtotime(date("Y-m-d" , time()) . "11:30:00");
 		$nowtime = strtotime(date("Y-m-d H:i:s", time()));
 		$customer_name = $this->input->post('customer_name');
@@ -104,19 +108,18 @@
 		$restaurant_list = $this->meal_book->get_restaurant();
  		$arrProject = $this->common->generateSelPjByid_value($restaurant_list,"project",$project);
  		$data['project'] = $arrProject;
-/*		if($nowtime > $starttime && $nowtime < $endtime)
-		{*/
- 		$data_list = $this->input->post('data_list');
- 		$this->meal_book->insert_orderlist($data_list);
-  		//$this->form_validation->set_rules('customer_name','您的大名', 'required');
- 		$customer_name = $this->input->post('customer_name');
- 		$this->load->view('/meal/meal_book',$data);
- 		//}
-/* 		else
+		if($nowtime > $starttime && $nowtime < $endtime)
+		{
+ 			$data_list = $this->input->post('data_list');
+ 			$this->meal_book->insert_orderlist($data_list);
+ 			$customer_name = $this->input->post('customer_name');
+ 			$this->load->view('/meal/meal_book',$data);
+ 		}
+ 		else
  		{
  			$data['customer_name'] = $customer_name;
  			$this->load->view('/meal/meal_error',$data);
- 		}*/
+ 		}
  	}
 
 	function meal_book_cancel()
@@ -129,6 +132,16 @@
  	{
  		$this->meal_book->meal_statistics();
  	}	
+
+ 	function readfromfile($file)
+ 	{
+ 	    $filename = "./hrg_config/$file";
+    	$handle = fopen($filename, "r");//读取二进制文件时，需要将第二个参数设置成'rb'
+    	//通过filesize获得文件大小，将整个文件一下子读到一个字符串中
+    	$contents = fread($handle, filesize ($filename));
+    	fclose($handle);
+    	return $contents;
+ 	}
 }
 
 ?>
